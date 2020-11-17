8d2745a7f1e41161b9c7e1e0409b9529dfcbe03c
spes2357 <spes2357@gmail.com>
2020-11-16 19:28:13 -0600

    Update2 11/16/2020

--- naivebayesNLP.py
+++ naivebayesNLP.py
@@ -10,7 +10,9 @@ from sklearn.naive_bayes import MultinomialNB
 from sklearn.model_selection import train_test_split
 from collections import OrderedDict
 import os
-
+from sklearn.model_selection import KFold
+import numpy as np
+from sklearn.feature_extraction.text import TfidfVectorizer
 plt.style.use('ggplot')
 nltk.download('punkt')
 nltk.download('stopwords')
@@ -48,6 +50,19 @@ class NativeBayesNLP:
         X_train, X_test, Y_train, Y_test, idx_train, idx_test = train_test_split(X, Y, indices, test_size=0.15)
         return X_train, X_test, Y_train, Y_test, idx_train, idx_test
 
+    def preprocessingXY(self):
+
+        self.checkfilesCounts(self.data_path_d)
+        self.checkfilesCounts(self.data_path_nd)
+        self.df = self.getTextFromFiles(self.df, self.data_path_d, self.depressionClass, self.numberOfDFiles)
+        self.df = self.getTextFromFiles(self.df, self.data_path_nd, self.nonDepressionClass, self.numberOfNDFiles)
+        self.dataPreprocessingForX(self.df, 'text')
+        self.dataPreprocessingForY(self.df, 'depression')
+        X = self.df['text'].to_numpy()
+        Y = self.df['depression'].to_numpy()
+
+        return X, Y
+
 
     def getTextFromFiles(self, df, data_path, zeroOrOne , limit):
         """Return Data Frame """
@@ -64,6 +79,7 @@ class NativeBayesNLP:
     def dataPreprocessingForX(self, df, columnName1):
         df[columnName1] = df[columnName1].map(lambda text: text.lower())
         df[columnName1] = df[columnName1].map(lambda text: nltk.tokenize.word_tokenize(text))
+        print(df[columnName1])
         stop_words = set(nltk.corpus.stopwords.words('english'))
         df[columnName1] = df[columnName1].map(lambda tokens: [w for w in tokens if not w in stop_words])
         df[columnName1] = df[columnName1].map(lambda text: ' '.join(text))
@@ -108,9 +124,9 @@ class NativeBayesNLP:
 
         plt.show()
 
-    def fitandPredict(self, X_train, Y_train, X_test, ngram_range=(1, 3), min_df=50):
+    def fitandPredict(self, X_train, Y_train, X_test, ngram_range=(1, 3), min_df=50, max_df= 0.95):
 
-        count_vectorizer = CountVectorizer(ngram_range = ngram_range, min_df = min_df)
+        count_vectorizer = CountVectorizer(ngram_range = ngram_range, min_df = min_df, max_df = max_df )
         self.counts = count_vectorizer.fit_transform(X_train)
         self.featnames = count_vectorizer.get_feature_names()
         self.classifier.fit(self.counts, Y_train)
@@ -171,14 +187,101 @@ class NativeBayesNLP:
         plt.savefig("NonDepressionWordCloud.png")
         plt.show()
 
+    def kfoldfitpredict(self, X, Y,ngram_range=(1, 5), min_df=50, max_df= 0.95):
+        inputs = X
+        targets = Y
+        num_folds = 5
+        # Define the K-fold Cross Validator
+        kfold = KFold(n_splits=num_folds, shuffle=True)
+        acc_per_fold = []
+        # K-fold Cross Validation model evaluation
+        fold_no = 1
+        for train, test in kfold.split(inputs, targets):
+
+            count_vectorizer = CountVectorizer(ngram_range=ngram_range, min_df=min_df, max_df=max_df)
+            self.counts = count_vectorizer.fit_transform(inputs[train])
+            self.featnames = count_vectorizer.get_feature_names()
+            print(len(self.featnames))
+            self.classifier.fit(self.counts, targets[train])
+            self.test_counts = count_vectorizer.transform(inputs[test])
+            predictions = self.classifier.predict(self.test_counts)
+            score  = self.score(predictions, targets[test])
+
+            print(
+                f'Score for fold {fold_no}: Accruacy of {score * 100}%')
+            acc_per_fold.append(score * 100)
+
+
+            # Increase fold number
+            fold_no = fold_no + 1
+
+        # == Provide average scores ==
+        print('------------------------------------------------------------------------')
+        print('Score per fold')
+        for i in range(0, len(acc_per_fold)):
+            print('------------------------------------------------------------------------')
+            print(f'> Fold {i + 1} - - Accuracy: {acc_per_fold[i]}%')
+        print('------------------------------------------------------------------------')
+        print('Average scores for all folds:')
+        print(f'> Accuracy: {np.mean(acc_per_fold)} (+- {np.std(acc_per_fold)})')
+        print('------------------------------------------------------------------------')
+    def kfoldfitpredictTFIDF(self, X, Y,ngram_range=(1, 5), min_df=50, max_df= 0.95):
+        inputs = X
+        targets = Y
+        num_folds = 5
+        # Define the K-fold Cross Validator
+        kfold = KFold(n_splits=num_folds, shuffle=True)
+        acc_per_fold = []
+        # K-fold Cross Validation model evaluation
+        fold_no = 1
+        for train, test in kfold.split(inputs, targets):
+
+            # count_vectorizer = CountVectorizer(ngram_range=ngram_range, min_df=min_df, max_df=max_df)
+            vectorizer = TfidfVectorizer(ngram_range=ngram_range, min_df=min_df, max_df=max_df)
+            self.counts = vectorizer.fit_transform(inputs[train])
+            self.featnames = vectorizer.get_feature_names()
+            print(len(self.featnames))
+            # tfidf_vectorizer = TfidfTransformer().fit(self.counts)
+            self.classifier.fit(self.counts, targets[train])
+            # example_tfidf = tfidf_vectorizer.transform(self.test_counts)
+            self.test_counts = vectorizer.transform(inputs[test])
+            predictions = self.classifier.predict(self.test_counts)
+            score  = self.score(predictions, targets[test])
+
+            print(
+                f'Score for fold {fold_no}: Accruacy of {score * 100}%')
+            acc_per_fold.append(score * 100)
+
+
+            # Increase fold number
+            fold_no = fold_no + 1
+
+        # == Provide average scores ==
+        print('------------------------------------------------------------------------')
+        print('Score per fold')
+        for i in range(0, len(acc_per_fold)):
+            print('------------------------------------------------------------------------')
+            print(f'> Fold {i + 1} - - Accuracy: {acc_per_fold[i]}%')
+        print('------------------------------------------------------------------------')
+        print('Average scores for all folds:')
+        print(f'> Accuracy: {np.mean(acc_per_fold)} (+- {np.std(acc_per_fold)})')
+        print('------------------------------------------------------------------------')
 
 if __name__ == "__main__":
     obj = NativeBayesNLP(numberOfDFiles =1000, numberOfNDFiles=500, data_path_d="reddit_depression",
                      data_path_nd="reddit_non_depression")
-    X_train, X_test, Y_train, Y_test, idx_train, idx_test = obj.preprocessing()
-    predictions = obj.fitandPredict( X_train, Y_train, X_test, ngram_range=(1, 5), min_df=50)
-    obj.score(predictions ,Y_test)
-    obj.makeWorldCloud(idx_test, predictions)
-    obj.countBarChart()
+    # X_train, X_test, Y_train, Y_test, idx_train, idx_test = obj.preprocessing()
+    # predictions = obj.fitandPredict( X_train, Y_train, X_test, ngram_range=(1, 5), min_df=50,  max_df= 0.90)
+    # obj.score(predictions ,Y_test)
+    # obj.makeWorldCloud(idx_test, predictions)
+    # print(len(obj.featnames))
+
+    X, Y = obj.preprocessingXY()
+    obj.kfoldfitpredict(X,Y)
+    # obj.kfoldfitpredictTFIDF(X, Y, ngram_range=(2, 5), min_df=50, max_df=0.95)
+    obj.kfoldfitpredictTFIDF(X, Y, ngram_range=(1, 5), min_df = 50, )
+
+    # obj.countBarChart()
     # predictions2 = obj.addtionalFitandPredictForTFIDF(Y_train)
     # obj.score(predictions2 ,Y_test)
+    
